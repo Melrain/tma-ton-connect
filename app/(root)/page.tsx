9cc10864c22ff04ac359ce1b270c304f0c516001
Melrain <yezyd92@icloud.com>
2025-02-09 00:29:36 +0800

    1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,17 @@
+"use client";
 import TonButton from "@/components/TonButton";
 import UserInfo from "@/components/UserInfo";
+import { useIsConnectionRestored } from "@tonconnect/ui-react";
 import React from "react";
 
 const page = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const connectionRestored = useIsConnectionRestored();
+  if (!connectionRestored) {
+    return <div>Please wait...</div>;
+  }
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full h-screen overflow-hidden">
       <div className="flex w-full justify-end p-2">
         <TonButton />
       </div>
--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -1,16 +1,55 @@
 "use client";
 
-import { useTonAddress } from "@tonconnect/ui-react";
+import {
+  CHAIN,
+  SendTransactionRequest,
+  useTonAddress,
+  useTonConnectUI,
+} from "@tonconnect/ui-react";
+import { Cell, Address, beginCell, storeMessage, TonClient } from "@ton/ton";
+
 import React from "react";
+import { retry } from "@/utils";
+
+const transaction: SendTransactionRequest = {
+  validUntil: Date.now() + 1 * 60 * 1000, // 5 minutes
+  messages: [
+    {
+      address: "0QD-SuoCHsCL2pIZfE8IAKsjc0aDpDUQAoo-ALHl2mje04A-", // message destination in user-friendly format
+      amount: "20000000", // Toncoin in nanotons
+    },
+  ],
+  network: CHAIN.TESTNET,
+};
 
 const UserInfo = () => {
+  const [status, setStatus] = React.useState("Idle");
   const userFriendlyAddress = useTonAddress();
-  const rawAddress = useTonAddress(false);
+  const [tonConnectUI, setOptions] = useTonConnectUI();
+
+  const onSendTon = async () => {
+    try {
+      // Set loading state
+      tonConnectUI.setConnectRequestParameters({ state: "loading" });
+      const result = await tonConnectUI.sendTransaction(transaction);
+      const exBoc = result.boc;
+      const client = new TonClient({
+        endpoint: "https://toncenter.com/api/v2/jsonRPC",
+        apiKey:
+          "fb067e269eb882e96e68e37d26c7c520d2bb362334e290478c0cc91b76f120e1",
+      });
+
+      const myAddress = Address.parse(userFriendlyAddress);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     userFriendlyAddress && (
-      <div>
-        <span>User-friendly address: {userFriendlyAddress}</span>
-        <span>Raw address: {rawAddress}</span>
+      <div className="flex flex-col flex-wrap items-center justify-center">
+        <span className="break-words text-xs">{userFriendlyAddress}</span>
+        <button onClick={onSendTon}>Send Ton</button>
+        <span>{status}</span>
       </div>
     )
   );
