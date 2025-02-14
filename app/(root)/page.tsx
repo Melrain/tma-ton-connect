168d47a24a015adcb32e5b1b53a20d527e4aeaa2
Melrain <yezyd92@icloud.com>
2025-02-14 15:50:55 +0800

    1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,7 @@
-import TonConnectButtonComponent from "@/components/shared/TonConnectButtonComponent";
-
-import UserInfo from "@/components/UserInfo";
 import React from "react";
 
 const page = () => {
-  return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <TonConnectButtonComponent />
-      <UserInfo />
-    </div>
-  );
+  return <div className="flex min-h-screen"></div>;
 };
 
 export default page;
--- components/AllPokerRooms.tsx
+++ components/AllPokerRooms.tsx
@@ -1,22 +1,12 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { useTonWallet } from "@tonconnect/ui-react";
-import { backButton } from "@telegram-apps/sdk-react";
+
 import { useRouter } from "next/navigation";
 
 const AllPokerRooms = () => {
   const router = useRouter();
-
-  useEffect(() => {
-    const initBackButton = async () => {
-      if (!backButton.isMounted()) {
-        backButton.mount();
-        backButton.show();
-      }
-    };
-    initBackButton();
-  }, []);
 
   if (!useTonWallet()) {
     router.push("/");
