d7c021c9f65567c585186a26ca0dfd745038d6de
Melrain <yezyd92@icloud.com>
2025-02-16 22:01:11 +0800

    auth

--- components/AllPokerRooms.tsx
+++ components/AllPokerRooms.tsx
@@ -1,18 +1,60 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTonWallet } from "@tonconnect/ui-react";
 
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { BiArrowBack } from "react-icons/bi";
+import { io } from "socket.io-client";
 
 const AllPokerRooms = () => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
   const router = useRouter();
 
   if (!useTonWallet()) {
     router.push("/");
   }
+
+  useEffect(() => {
+    const socketURL = process.env.NEXT_PUBLIC_SOCKET_URL;
+
+    const socket = io(socketURL, {
+      extraHeaders: {
+        username: "admin",
+        tonwallet: "address",
+        roomid: "1",
+      },
+    });
+
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on("upgrade", (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
 
   return (
     <div className="flex w-full flex-col">
@@ -20,6 +62,12 @@ const AllPokerRooms = () => {
         <Link href={"/"}>
           <BiArrowBack />
         </Link>
+      </div>
+      {/* socket info */}
+      <div>
+        <h1>Socket Info</h1>
+        <div>Connected: {isConnected ? "Yes" : "No"}</div>
+        <div>Transport: {transport}</div>
       </div>
     </div>
   );
--- components/forms/CreateRoomForm.tsx
+++ components/forms/CreateRoomForm.tsx
@@ -2,7 +2,7 @@
 
 import { useTonWallet } from "@tonconnect/ui-react";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -23,7 +23,10 @@ import {
   SelectValue,
 } from "../ui/select";
 import { Button } from "../ui/button";
-import { socket } from "@/lib/socket";
+import Link from "next/link";
+import { BiArrowBack } from "react-icons/bi";
+import axios from "axios";
+import { retrieveLaunchParams } from "@telegram-apps/sdk";
 
 const formSchema = z.object({
   blindSize: z.coerce.number(),
@@ -31,10 +34,9 @@ const formSchema = z.object({
 });
 
 const CreateRoomForm = () => {
-  const [isConnected, setIsConnected] = useState(false);
-  const [transport, setTransport] = useState("N/A");
   const router = useRouter();
   const wallet = useTonWallet();
+  const { initDataRaw, initData } = retrieveLaunchParams();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -45,7 +47,27 @@ const CreateRoomForm = () => {
   });
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
-    console.log(data);
+    try {
+      console.log(data);
+      if (!initDataRaw) {
+        return alert(`No auth data found: ${initDataRaw}`);
+      }
+      // send auth to server
+      const authResponse = await axios.post(
+        "http://localhost:8080/api/auth/validate", // URL
+        {}, // Body 数据
+        {
+          headers: {
+            // 传递 HTTP headers
+            Authorization: `tma ${initDataRaw}`,
+          },
+        }
+      );
+
+      console.log(authResponse);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
@@ -54,95 +76,74 @@ const CreateRoomForm = () => {
     }
   }, [wallet, router]);
 
-  useEffect(() => {
-    if (socket.connected) {
-      onConnect();
-    }
-
-    function onConnect() {
-      setIsConnected(true);
-      setTransport(socket.io.engine.transport.name);
-
-      socket.io.engine.on("upgrade", (transport) => {
-        setTransport(transport.name);
-      });
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-      setTransport("N/A");
-    }
-
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-    };
-  }, []);
-
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-6 px-10 flex justify-center items-center flex-col min-h-screen">
-        {/* blindSize */}
-        <FormField
-          control={form.control}
-          name="blindSize"
-          render={({ field }) => (
-            <FormItem className="w-1/2">
-              <FormLabel>BlindSize</FormLabel>
-              <Select
-                onValueChange={field.onChange}
-                defaultValue={field.value.toString()}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a verified email to display" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent className="text-white">
-                  <SelectItem value="1">1</SelectItem>
-                  <SelectItem value="5">5</SelectItem>
-                  <SelectItem value="10">10</SelectItem>
-                </SelectContent>
-              </Select>
-              <FormDescription>
-                The size of the blinds in the room.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {/* seats */}
-        <FormField
-          control={form.control}
-          name="maxPlayers"
-          render={({ field }) => (
-            <FormItem className="w-1/2">
-              <FormLabel>maxPlayers</FormLabel>
-              <Select
-                onValueChange={field.onChange}
-                defaultValue={field.value.toString()}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a verified email to display" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent className="text-white">
-                  <SelectItem value="6">6</SelectItem>
-                  <SelectItem value="9">9</SelectItem>
-                </SelectContent>
-              </Select>
-              <FormDescription>maxPlayers</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Create Room</Button>
-      </form>
-    </Form>
+    <div>
+      <div className="absolute top-0 left-0 py-10 px-2">
+        <Link href={"/"}>
+          <BiArrowBack />
+        </Link>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6 px-10 flex justify-center items-center flex-col min-h-screen">
+          {/* blindSize */}
+          <FormField
+            control={form.control}
+            name="blindSize"
+            render={({ field }) => (
+              <FormItem className="w-1/2">
+                <FormLabel>BlindSize</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value.toString()}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a verified email to display" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent className="text-white">
+                    <SelectItem value="1">1</SelectItem>
+                    <SelectItem value="5">5</SelectItem>
+                    <SelectItem value="10">10</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormDescription>
+                  The size of the blinds in the room.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* seats */}
+          <FormField
+            control={form.control}
+            name="maxPlayers"
+            render={({ field }) => (
+              <FormItem className="w-1/2">
+                <FormLabel>maxPlayers</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value.toString()}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a verified email to display" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent className="text-white">
+                    <SelectItem value="6">6</SelectItem>
+                    <SelectItem value="9">9</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormDescription>maxPlayers</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Create Room</Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 
